adc2b0b74a01cac765911c19e14905b7e4b9e650
Nikolay.Tropin <nikolay.tropin@jetbrains.com>
2020-02-26 12:26:01 +0300

    calculate test locations for ScalaTest in the same thread
    and use CachedInUserData annotation instead of custom caching
    
    LineMarkerProviders are run in background anyway, and restarting daemon breaks incremental highlighting
    #SCL-17097

--- scala/scala-impl/src/org/jetbrains/plugins/scala/testingSupport/test/scalatest/ScalaTestTestLocationsFinder.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/testingSupport/test/scalatest/ScalaTestTestLocationsFinder.scala
@@ -1,11 +1,12 @@
 package org.jetbrains.plugins.scala.testingSupport.test.scalatest
 
-import com.intellij.openapi.module.Module
+import com.intellij.openapi.progress.ProgressManager
 import com.intellij.psi.PsiElement
+import org.jetbrains.plugins.scala.caches.CachesUtil
 import org.jetbrains.plugins.scala.lang.psi.api.expr.{MethodInvocation => ScMethodInvocation, _}
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.templates.ScTemplateBody
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScTypeDefinition
-import org.jetbrains.plugins.scala.macroAnnotations.Measure
+import org.jetbrains.plugins.scala.macroAnnotations.CachedInUserData
 import org.scalatest.finders._
 
 object ScalaTestTestLocationsFinder {
@@ -13,11 +14,15 @@ object ScalaTestTestLocationsFinder {
   type TestLocations = Seq[PsiElement]
 
   /** should be called in a read action  */
-  @Measure
-  def calculateTestLocations(definition: ScTypeDefinition, module: Module): Option[TestLocations] = {
+  @CachedInUserData(definition, CachesUtil.fileModTracker(definition.getContainingFile))
+  def calculateTestLocations(definition: ScTypeDefinition): Option[TestLocations] = {
     //Thread.sleep(5000) // uncomment to test long resolve
-    val finder = ScalaTestAstTransformer.getFinder(definition, module)
-    finder.flatMap(doCalculateScalaTestTestLocations(definition, _))
+    for {
+      module <- definition.module
+      finder <- ScalaTestAstTransformer.getFinder(definition, module)
+      locations <- doCalculateScalaTestTestLocations(definition, finder)
+    } yield
+      locations
   }
 
   // NOTE 1:
